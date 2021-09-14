9647bf83e988a18ee733127f3ed0751e1fdecce8
HackerMan018 <bouadoutanoh9@gmail.com>
2021-09-14 13:03:08 +0000

    Mise a jour page acceuil

--- resources/views/includes/content.blade.php
+++ resources/views/includes/content.blade.php
@@ -3,40 +3,46 @@
       <div class="container">
 
         <div class="row">
-          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up">
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up">
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4 class="title"><a href="">Atouts</a></h4>
+              <h4 class="title"><a href="">Parteniare</a></h4>
                 <p class="description">
-                  At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
+                  5
                 </p>
             </div>
           </div>
 
-          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-file"></i></div>
-              <h4 class="title"><a href="">Expérience</a></h4>
+              <h4 class="title"><a href="">Clients satisfaits</a></h4>
               <p class="description">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
+                20
               </p>
             </div>
           </div>
 
-          <div class="col-md-6 col-lg-4 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="200">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-tachometer"></i></div>
-              <h4 class="title"><a href="">Efficacité</a></h4>
+              <h4 class="title"><a href="">Projet menés</a></h4>
               <p class="description">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque
+                20
               </p>
             </div>
           </div>
 
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-layer"></i></div>
+              <h4 class="title"><a href="">Projet en cours</a></h4>
+              <p class="description">5</p>
+            </div>
           </div>
-
+  
         </div>
-
       </div>
     </section><!-- End Icon Boxes Section -->
 
